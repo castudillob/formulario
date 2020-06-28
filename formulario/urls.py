444dc4ca59ba5c5e5e5787093ab9a4121a51533f
castudillob <castudil@gmail.com>
2020-06-28 16:52:57 -0400

    tercer envío jun 281652 2020

--- formulario/urls.py
+++ formulario/urls.py
@@ -1,9 +1,13 @@
 # pages/urls.py
-from django.urls import path
-
+from django.contrib import admin
+from django.urls import include, path
+from django.contrib.auth import views as auth_views
+from . import contact
 from .views import HomePageView, AboutPageView # new
 
 urlpatterns = [
-    path('about/', AboutPageView.as_view(), name='about'), # new
-    path('', HomePageView.as_view(), name='home'),
+     path('admin/', admin.site.urls),
+     path('contact/', contact.contact, name='contact'),
+     path('about/', AboutPageView.as_view(), name='about'), # new
+     path('', HomePageView.as_view(), name='home'),
 ]
